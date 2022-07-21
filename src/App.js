3632810b42dc6c9ccc66736bcb5430aa4d44b9ef
PT71639590 (Huamani Ccahuay <pt71639590@idat.edu.pe>
2022-07-21 14:37:40 -0500

    feat: create route for the Profile page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import News from './pages/News';
+import Profile from './pages/Profile';
 
 function App() {
 	return (
@@ -18,6 +19,7 @@ function App() {
 						<Route path="/about" element={<About />} />
 						<Route path="/contact" element={<Contact />} />
 						<Route path="/news" element={<News />} />
+						<Route path="/my-profile" element={<Profile />} />
 					</Routes>
 				</div>
 			</div>
