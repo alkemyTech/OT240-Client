b46d63cb804b97e0a3792524bcfebd5b570659cc
emiliof <emiliofurrer00@gmail.com>
2022-07-30 04:41:26 -0300

    added mock contacts state && deleteContact function

--- src/pages/Backoffice/Contacts.jsx
+++ src/pages/Backoffice/Contacts.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contacts() {
+  //This should be replaced with Redux state logic once it is available
+  const [contacts, setContacts] = useState(mockContacts);
+
+  function deleteContact(id) {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  }
+
   return (
     <div>
+      <h1>Contactos</h1>
       <table className='table'>
         <thead>
           <th>Nombre</th>
@@ -11,19 +19,46 @@ function Contacts() {
           <th>Acciones</th>
         </thead>
         <tbody>
-          <tr>
-            <td>Juan Perez</td>
-            <td>+543795854695</td>
-            <td>juanperez@hotmail.com</td>
-            <td>
-                <button className="btn btn-outline-primary">Ver Mensaje</button>
-                <button className="btn btn-outline-danger">Eliminar</button>
-            </td>
-          </tr>
+          {contacts.length &&
+            contacts.map(({ name, phone, email, message, id }) => (
+              //Pending: add expandable message section
+              <tr>
+                <td>{name}</td>
+                <td>{phone}</td>
+                <td>{email}</td>
+                <td>
+                  <button className='btn btn-outline-primary'>Ver Mensaje</button>
+                  <button onClick={() => deleteContact(id)} className='btn btn-outline-danger'>
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
   );
 }
+
+const mockContacts = [
+  {
+    name: 'Juan Perez',
+    phone: '+543795854695',
+    email: 'juan@hotmail.com',
+    id: 1,
+  },
+  {
+    name: 'Natalia Fernandez',
+    phone: '+543794254595',
+    email: 'natif@hotmail.com',
+    id: 2,
+  },
+  {
+    name: 'Romina Gutierrez',
+    phone: '+543395854885',
+    email: 'rominaa@yahoo.com.ar',
+    id: 3,
+  },
+];
 
 export default Contacts;
