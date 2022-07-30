5cbc675ecf71716b373e6e2c291544f4c6ba9b46
emiliof <emiliofurrer00@gmail.com>
2022-07-30 04:24:02 -0300

    added basic hardcoded contacts table

--- src/pages/Backoffice/Contacts.jsx
+++ src/pages/Backoffice/Contacts.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <div>
+      <table className='table'>
+        <thead>
+          <th>Nombre</th>
+          <th>Telefono</th>
+          <th>Email</th>
+          <th>Acciones</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Juan Perez</td>
+            <td>+543795854695</td>
+            <td>juanperez@hotmail.com</td>
+            <td>
+                <button className="btn btn-outline-primary">Ver Mensaje</button>
+                <button className="btn btn-outline-danger">Eliminar</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Contacts;
