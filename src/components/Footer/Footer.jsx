81119810cabcf0997c13cde4b58ad7176d85a50a
Andres Nazzari <lordcliff@gmail.com>
2022-08-01 14:25:33 +0900

    create component file and style

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./styles/Footer.module.scss"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div></div>
+    </footer>
+  )
+}
+
+export default Footer
