56f36d2861d7c5ddfe6de6dd9beef86923d46727
Andres Nazzari <lordcliff@gmail.com>
2022-08-01 16:31:45 +0900

    💄 UI: Added footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import style from "./styles/Footer.module.scss"
+import React from 'react';
+import style from './styles/Footer.module.scss';
+import LogoFooter from '../LogoFooter/LogoFooter';
+import Links from '../Links/Links';
+import RedesIcons from '../RedesIcons/RedesIcons';
 
 const Footer = () => {
   return (
-    <footer>
-      <div></div>
+    <footer className={style.footer}>
+      <LogoFooter />
+      <Links />
+      <RedesIcons />
+      <p>2022 by Alkemy. All Rights Reserved.</p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
