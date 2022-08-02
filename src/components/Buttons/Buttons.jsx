4a6ec10e9682fd2d4922d1d823992318fc71b6a9
Andres Nazzari <lordcliff@gmail.com>
2022-08-02 16:33:03 +0900

    💄 UI: login/register, backoffice/logout buttons

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import style from './styles/Buttons.module.scss';
+import { Link } from 'react-router-dom';
+
+const Buttons = () => {
+  //estos se tomaran desde el state o local storage!!!
+  const isAdmin = false;
+  const isAuth = false;
+
+  return (
+    <div className={style.buttons}>
+      {!isAuth && (
+        <>
+          <div className={style.btn}>
+            <Link to={'/login'}>Log in</Link>
+          </div>
+          <div className={`${style.btn} ${style.registerBtn}`}>
+            <Link to={'/registro'}>Registrate</Link>
+          </div>
+        </>
+      )}
+      {isAdmin && isAuth && (
+        <div className={style.btn}>
+          <Link to={'/backoffice'}>Backoffice</Link>
+        </div>
+      )}
+      {isAuth && (
+        <div className={style.btn}>
+          <Link to={'/'}>Log out</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Buttons;
