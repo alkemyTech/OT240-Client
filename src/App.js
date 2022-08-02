eee1c771111faafb66290111e035f53f2aa2c807
emiliof <emiliofurrer00@gmail.com>
2022-08-02 17:29:00 -0300

    added temporary backoffice/contacts route for development purposes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import Contact from './pages/Contact';
 import News from './pages/News';
 import Profile from './pages/Profile';
 import Footer from './components/Footer/Footer';
+import Contacts from './pages/Backoffice/Contacts';
 
 function App() {
   return (
     <Router>
-      {/* <Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/contact" element={<Contact />} />
-						<Route path="/news" element={<News />} />
-						<Route path="/my-profile" element={<Profile />} />
-					</Routes> */}
+      {
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/news' element={<News />} />
+          <Route path='/my-profile' element={<Profile />} />
+          <Route path='/backoffice/contacts' element={<Contacts />} />
+        </Routes>
+      }
       <Footer />
     </Router>
   );
