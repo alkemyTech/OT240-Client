576af461201260bc0505e99a4014f0af4fab3d3e
Tomas Paez <paeztms@gmail.com>
2022-08-03 01:59:20 -0300

    add component routed to /novedades in public router

--- src/pages/Public/Public.jsx
+++ src/pages/Public/Public.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 // import Header from '../../components/Header/Header.jsx';
+import News from './News/News.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
 
 const Public = () => {
@@ -8,12 +9,12 @@ const Public = () => {
     <>
       {/* <Header /> */}
       <Routes>
+        <Route path='/novedades' element={<News />} />
         {/* <Route path='/' element={<Home />} />
           <Route path='/login' element={<Testimonials />} />
           <Route path='/registro' element={<Testimonials />} />
           <Route path='/testimonios' element={<Testimonials />} />
           <Route path='/nosotros' element={<News />} />
-          <Route path='/novedades' element={<News />} />
           <Route path='/contacto' element={<News />} />
           <Route path='/contribuye' element={<News />} />
           <Route path='/staff' element={<News />} /> */}
