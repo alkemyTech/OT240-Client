961a48e889716797c0382d1f05b91606d88bcd29
Mauricio <mauri.yapura@yahoo.com>
2022-08-03 17:09:18 -0300

    configuracion de ruta

--- src/pages/Backoffice/Backoffice.jsx
+++ src/pages/Backoffice/Backoffice.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { TestimonialBackoffice } from '../../components/testimonials/Testimonial';
 
 const Backoffice = () => {
   return (
     // LayoutBackoffice
-    <Router>
+    
       <Routes>
         {/* <Route path='/' element={<Backoffice />} />
         <Route path='/backoffice' element={<Backoffice />} /> */}
+        <Route path='/testimonios' element={<TestimonialBackoffice />} />
       </Routes>
-    </Router>
+    
   );
 };
 
