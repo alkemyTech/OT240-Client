a4296dc7bdab68656608e1036a427aca4246d4dd
Andres Nazzari <lordcliff@gmail.com>
2022-08-03 07:39:06 -0300

    🚧 PROGRESS: work in progress

---
+++ src/components/BackofficeLayout/BackofficeLinks/BackofficeLinks.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './style/BackofficeLinks.module.scss';
+
+const BackofficeLinks = () => {
+  return <div className='BackofficeLinksWrapper'>Test content</div>;
+};
+
+export default BackofficeLinks;
