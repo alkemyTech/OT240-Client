8c437b02fa05a8393daa900cb5b050e0724e4df3
Andres Nazzari <lordcliff@gmail.com>
2022-08-04 06:36:22 -0300

    ✨NEW FEATURE: added categories route to backoffice

--- src/pages/Backoffice/Backoffice.jsx
+++ src/pages/Backoffice/Backoffice.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import BackofficeCards from '../../components/BackofficeCards/BackofficeCards.jsx';
-
+import Categories from '../../components/Categories/Categories';
 const Backoffice = () => {
   return (
     // LayoutBackoffice
@@ -10,6 +10,7 @@ const Backoffice = () => {
       <Header />
       <Routes>
         <Route path='/' element={<BackofficeCards />} />
+        <Route path='/categories' element={<Categories />} />
         {/*<Route path='/backoffice' element={<Backoffice />} /> */}
       </Routes>
     </>
