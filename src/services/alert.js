e3570ee51ef3da907d3dfe48ce8683db97716300
emiliof <emiliofurrer00@gmail.com>
2022-08-04 09:34:15 -0300

    refactored and commented alert.js function

--- src/services/alert.js
+++ src/services/alert.js
@@ -1,10 +1,23 @@
 import Swal from 'sweetalert2';
 
-const showAlert = ({ title, text, icon }) => {
+/**
+ * Fires a Swal alert.
+ *
+ * @param {{
+ *    title: string,
+ *    text: string,
+ *    icon: 'success' | 'error'	| 'warning'	| 'info' | 'question'
+ *  }} config -
+
+ * @param {object} extraParams - Any valid sweetalert2 fire() arguments.
+ */
+
+const showAlert = ({ title, text, icon }, extraParams = {}) => {
   Swal.fire({
     title,
     text,
     icon,
+    ...extraParams,
   });
 };
 
