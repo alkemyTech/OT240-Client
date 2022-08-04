9a1519eb81e361991d64ac18fdbd3a20c4ba0186
Andres Nazzari <lordcliff@gmail.com>
2022-08-04 04:47:25 -0300

    🧹 CLEAN: deleted unused components

--- src/components/BackofficeLayout/BackofficeLinks/BackofficeLinks.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './style/BackofficeLinks.module.scss';
-
-const BackofficeLinks = () => {
-  return <div className='BackofficeLinksWrapper'>Test content</div>;
-};
-
-export default BackofficeLinks;
--- src/components/BackofficeNavBar/BackofficeNavBar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import style from './styles/BackofficeNavBar.module.scss';
-
-const BackofficeNavBar = (props) => <div className='BackofficeNavBarWrapper'>Test content</div>;
-
-export default BackofficeNavBar;
