240771ef277f40801e777630e23ce7e965edc7f0
Mauricio <mauri.yapura@yahoo.com>
2022-08-03 22:53:51 -0300

    routes configuration

--- src/pages/Backoffice/Backoffice.jsx
+++ src/pages/Backoffice/Backoffice.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ActivitiesBackoffice } from '../../components/activities/ActivitiesBackoffice';
 
 const Backoffice = () => {
   return (
     // LayoutBackoffice
-    <Router>
+    
       <Routes>
         {/* <Route path='/' element={<Backoffice />} />
         <Route path='/backoffice' element={<Backoffice />} /> */}
+
+        <Route path='/actividades' element={<ActivitiesBackoffice />} /> 
       </Routes>
-    </Router>
+    
   );
 };
 
