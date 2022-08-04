5c4bdf97f82c631d673e9af6f6b710099f9cbd6d
Andres Nazzari <lordcliff@gmail.com>
2022-08-04 04:47:51 -0300

    ✨ NEW FEATURE: backoffice layout route

--- src/pages/Backoffice/Backoffice.jsx
+++ src/pages/Backoffice/Backoffice.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from '../../components/Header/Header';
+import BackofficeCards from '../../components/BackofficeCards/BackofficeCards.jsx';
 
 const Backoffice = () => {
   return (
@@ -8,8 +9,8 @@ const Backoffice = () => {
     <>
       <Header />
       <Routes>
-        {/* <Route path='/' element={<Backoffice />} />
-        <Route path='/backoffice' element={<Backoffice />} /> */}
+        <Route path='/' element={<BackofficeCards />} />
+        {/*<Route path='/backoffice' element={<Backoffice />} /> */}
       </Routes>
     </>
   );
