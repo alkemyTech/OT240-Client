df601fce55a9a1083d060094554fea0999f10d6b
Tomas Paez <paeztms@gmail.com>
2022-08-07 17:14:49 -0300

    reformat service to recieve options object instead of positional arguments

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,17 +4,18 @@ const ong_api = axios.create({
   baseURL: 'http://localhost:3000/api',
 });
 
-const fetchApi = async (method, url, data, params) => {
+const fetchApi = async ({ method, url, data = {}, params = {}, headers = {} }) => {
   const options = {
     method,
     url,
     data,
     params,
+    headers,
   };
 
   const authToken = sessionStorage.getItem('token');
 
-  authToken && (options.headers = { Authorization: `Bearer ${authToken}` });
+  authToken && (options.headers.Authorization = `Bearer ${authToken}`);
 
   return await ong_api(options);
 };
