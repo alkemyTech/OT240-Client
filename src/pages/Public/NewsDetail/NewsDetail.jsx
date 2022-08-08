499bb2ed74cf744567f4a6c6b0db2f73a08a3cc7
Tomas Paez <paeztms@gmail.com>
2022-08-08 04:02:54 -0300

    add fetching implementation on news detail to show real persisted news on db

--- src/pages/Public/NewsDetail/NewsDetail.jsx
+++ src/pages/Public/NewsDetail/NewsDetail.jsx
@@ -3,22 +3,44 @@ import { useParams, useNavigate } from 'react-router-dom';
 
 import style from './styles/NewsDetail.module.scss';
 
-import news from '../../../news.mock';
+import fetchApi from '../../../axios/axios';
 
 const NewsDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const currentNew = news.filter((entry) => entry.id == id)[0];
+
+  const [currentNew, setCurrentNew] = React.useState('');
+  const [error, setError] = React.useState('');
+
+  React.useEffect(() => {
+    let isMounted = true;
+    const getNew = async (id) => {
+      try {
+        const { data } = await fetchApi({ method: 'get', url: `/news/${id}` });
+        isMounted && setCurrentNew(data);
+      } catch (err) {
+        isMounted && setError(err.message);
+      }
+    };
+
+    getNew(id);
+
+    return () => (isMounted = false);
+  }, []);
 
   return (
-    <article className={style.container}>
-      <img src={currentNew.image} alt='' />
-      <div>
-        <h1>{currentNew.name}</h1>
-        <p>{currentNew.content}</p>
-        <button onClick={() => navigate(-1)}>Ir al inicio</button>
-      </div>
-    </article>
+    <>
+      {currentNew && (
+        <article className={style.container}>
+          <img src={`http://localhost:3000/img/news/${currentNew.image}`} alt='' />
+          <div>
+            <h1>{currentNew.name}</h1>
+            <p dangerouslySetInnerHTML={{ __html: currentNew.content }}></p>
+            <button onClick={() => navigate('/')}>Ir al inicio</button>
+          </div>
+        </article>
+      )}
+    </>
   );
 };
 
