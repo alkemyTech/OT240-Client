3c5a63319bf0796cde05089a3896503fc1a155a7
Tomas Paez <paeztms@gmail.com>
2022-08-08 04:00:55 -0300

    add real fetching implementation to NewsList component

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -3,19 +3,44 @@ import { useNavigate } from 'react-router-dom';
 
 import style from './styles/NewsList.module.scss';
 
-import news from '../../news.mock';
+import fetchApi from '../../axios/axios';
 
 const NewsList = ({ quantity }) => {
   const navigate = useNavigate();
+
+  const [news, setNews] = React.useState([]);
+  const [error, setError] = React.useState('');
+
+  React.useEffect(() => {
+    let isMounted = true;
+
+    const getNews = async () => {
+      try {
+        const { data } = await fetchApi({ method: 'get', url: '/news' });
+        isMounted && setNews(data);
+      } catch (err) {
+        setError('');
+      }
+    };
+
+    getNews();
+
+    return () => (isMounted = false);
+  }, []);
 
   return (
     <ul className={style.container}>
       {news.length ? (
         news.slice(0, quantity).map(({ name, image, id, content }) => (
           <li className={style.card} key={id}>
-            <img src={image} alt='' />
+            <div
+              className={style.img}
+              style={{
+                backgroundImage: `url('http://localhost:3000/img/news/${image}')`,
+              }}></div>
             <div>
-              <p>{content.slice(0, 100)}...</p>
+              <h1>{name}</h1>
+              <p>{content.slice(0, 50).replace(/<\/?[^>]+(>|$)/g, '')}...</p>
               <button className={style.buttonLink} onClick={() => navigate(`${id}`)}>
                 Ver Novedad
               </button>
