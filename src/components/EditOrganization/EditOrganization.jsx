a824f37d6af4e93b70e209151a03b2016aa8f1ea
emiliof <emiliofurrer00@gmail.com>
2022-08-08 11:56:37 -0300

    added some styling for layout and positioning

--- src/components/EditOrganization/EditOrganization.jsx
+++ src/components/EditOrganization/EditOrganization.jsx
@@ -9,21 +9,30 @@ function EditOrganization() {
     image: Yup.string().required(),
   });
   return (
-    <div>
-      <Formik
-        initialValues={{
-          name: '',
-          image: '',
-        }}
-        validationSchema={validationSchema}>
-        {({ values, handleChange, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <input type='text' name='name' value={values.name} onChange={handleChange} />
-            <input type='text' name='image' value={values.image} onChange={handleChange} />
-            <button type='submit'>Guardar cambios</button>
-          </form>
-        )}
-      </Formik>
+    <div className={styles.mainContainer}>
+      <h1>Editar Organización</h1>
+      <div className={styles.formContainer}>
+        <Formik
+          initialValues={{
+            name: '',
+            image: '',
+          }}
+          validationSchema={validationSchema}>
+          {({ values, handleChange, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <p>
+                <strong>Somos Más</strong>
+              </p>
+              <label htmlFor='name'>Nombre de la organización</label>
+              <input type='text' name='name' value={values.name} onChange={handleChange} />
+              <img src='http://localhost:3001/static/media/logo.7391c848.svg' />
+              <label htmlFor='image'>URL de imagen</label>
+              <input type='text' name='image' value={values.image} onChange={handleChange} />
+              <button type='submit'>Guardar cambios</button>
+            </form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
