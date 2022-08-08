57c197219365697814a546def2ecd5f29f3fe69d
emiliof <emiliofurrer00@gmail.com>
2022-08-08 11:29:40 -0300

    minimum form validations

--- src/components/EditOrganization/EditOrganization.jsx
+++ src/components/EditOrganization/EditOrganization.jsx
@@ -4,7 +4,28 @@ import * as Yup from 'yup';
 import styles from './styles/EditOrganization.module.scss';
 
 function EditOrganization() {
-  return <div>EditOrganization</div>;
+  const validationSchema = Yup.object({
+    name: Yup.string().required(),
+    image: Yup.string().required(),
+  });
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          name: '',
+          image: '',
+        }}
+        validationSchema={validationSchema}>
+        {({ values, handleChange, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <input type='text' name='name' value={values.name} onChange={handleChange} />
+            <input type='text' name='image' value={values.image} onChange={handleChange} />
+            <button type='submit'>Guardar cambios</button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default EditOrganization;
