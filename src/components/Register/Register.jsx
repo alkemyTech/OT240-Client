35d838cf0eb333476386631067bb70a3c18bdb94
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-09 19:32:24 -0300

    💄 UI: Register Fix
    Added another input with its validations,
    Changed login related text to register's text,
    Better responsive UI

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -7,46 +7,47 @@ import InputField from '../InputField/InputField';
 
 const Register = () => {
 
-  let formData = {};
-
   const validate = Yup.object({
-    registerName: Yup.string().required('El nombre es requerido').min(3, 'El nombre debe contener al menos 3 caracteres'),
+    registerFirstName: Yup.string().required('El nombre es requerido').min(3, 'El nombre debe contener al menos 3 caracteres'),
+    registerLastName: Yup.string().required('El apellido es requerido').min(3, 'El apellido debe contener al menos 3 caracteres'),
     registerEmail: Yup.string().required('El email es requerido').email('Email es invalido'),
     registerPassword: Yup.string().required('Contraseña es requerido').min(6, 'La contraseña debe contener al menos 6 caracteres'),
     registerConfirmPassword: Yup.string().required('Confirmar contraseña es requerido').oneOf([Yup.ref('registerPassword'), null], 'Las contraseñas no coinciden')
   });
 
+  const handleSubmit = async(values) => {
+
+    
+  };
+
   return (
     <div className={register.container}>
       <div className={register.formContainer}>
-          <p className={register.bienvenido}> Bienvenido </p>
-          <h4>Inicia sesión en tu cuenta!</h4>
+          <h3 className={register.bienvenido}> Bienvenido </h3>
+          <h4>Registrate!</h4>
           <Formik
             initialValues={{
-              registerName: '',
+              registerFirstName: '',
+              registerLastName: '',
               registerEmail: '',
               registerPassword: '',
               registerConfirmPassword: ''
             }}
             validationSchema={validate}
-            onSubmit={values => {
-              // IMPORTANT
-              // Handle API submission
-              formData = values;
-              console.log(formData); 
-            }}
+            onSubmit={handleSubmit}
           >
             {formik => (
               <Form onSubmit={formik.handleSubmit}>
-                  <InputField label='Nombre' name='registerName' type='text' />
+                  <InputField label='Nombre' name='registerFirstName' type='text' />
+                  <InputField label='Apellido' name='registerLastName' type='text' />
                   <InputField label='Email' name='registerEmail' type='email' />
                   <InputField label='Contraseña' name='registerPassword' type='Password' />
                   <InputField label='Confirmar contraseña' name='registerConfirmPassword' type='Password' />
-                  <button type='submit'> Inicia sesión </button>
+                  <button type='submit'> Registrarse </button>
               </Form>
             )}
           </Formik>
-          <p className={register.registro}>Ya tienes una cuenta? <a href='/registro'>Inicia sesión </a></p>
+          <p className={register.registro}>Ya tienes una cuenta? <a href='/login'>Inicia sesión </a></p>
       </div>
       <div className={register.imageContainer}></div>
     </div>
