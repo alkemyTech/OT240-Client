7a91edbbb264bf6a21d60ce254e29a53314ba06f
Tomas Paez <paeztms@gmail.com>
2022-08-10 02:05:40 -0300

    move file reader helpers to own file

--- src/components/Form/FileField.jsx
+++ src/components/Form/FileField.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 
+import { readFromBase64 } from '../../utils/convertToBase64';
+
 const FileField = ({ field, label, value, setState, style }) => {
   const imgPreviewRef = React.useRef();
 
-  const handlePreview = (image) => {
-    if (image && (image instanceof Blob || image instanceof ArrayBuffer)) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        imgPreviewRef.current.style.backgroundImage = `url('${reader.result}')`;
-      };
-      reader.readAsDataURL(image);
-    } else {
-      imgPreviewRef.current.style.backgroundImage = `url('${image}')`;
-    }
-  };
-
   React.useEffect(() => {
-    handlePreview(value);
+    readFromBase64(value, imgPreviewRef);
   });
 
   return (
@@ -30,7 +20,7 @@ const FileField = ({ field, label, value, setState, style }) => {
         onChange={(e) => {
           const { files } = e.target;
           setState((prev) => ({ ...prev, [field]: files[0] }));
-          handlePreview(files[0]);
+          readFromBase64(files[0], imgPreviewRef);
         }}
       />
       <div ref={imgPreviewRef} className={style.imgPreview}></div>
--- src/utils/convertToBase64.js
+++ src/utils/convertToBase64.js
@@ -1,7 +1,21 @@
-export const convertToBase64 = async (file) =>
+const convertToBase64 = async (file) =>
   new Promise((res, rej) => {
     const fileReader = new FileReader();
     fileReader.readAsDataURL(file);
     fileReader.onload = () => res(fileReader.result);
     fileReader.onerror = (error) => rej(error);
   });
+
+const readFromBase64 = (image, ref) => {
+  if (image && (image instanceof Blob || image instanceof ArrayBuffer)) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      ref.current.style.backgroundImage = `url('${reader.result}')`;
+    };
+    reader.readAsDataURL(image);
+  } else {
+    ref.current.style.backgroundImage = `url('${image}')`;
+  }
+};
+
+export { convertToBase64, readFromBase64 };
