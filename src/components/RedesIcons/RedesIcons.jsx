97819accddb3a43262592df8419c3ab08ae15920
emiliof <emiliofurrer00@gmail.com>
2022-08-11 15:20:08 -0300

    moved fetching logic to parent component and created state

--- src/components/RedesIcons/RedesIcons.jsx
+++ src/components/RedesIcons/RedesIcons.jsx
@@ -4,39 +4,25 @@ import FacebookIcon from './assets/facebook.png';
 import WhatsappIcon from './assets/whatsapp.png';
 import InstagramIcon from './assets/instagram.png';
 import MailIcon from './assets/gmail.png';
-import { useEffect } from 'react';
 
-const RedesIcons = () => {
-  fetchSocialMediaLinks().then(console.log);
-  useEffect(() => {});
-
+const RedesIcons = ({ socialMediaLinks }) => {
+  const { facebook, instagram, linkedin } = socialMediaLinks;
   return (
     <div className={style.socialMedia}>
       <a href={'mailto:somosfundacionmas@gmail.com'}>
         <img src={MailIcon} alt='mail_icon' />
       </a>
-      <a href={'https://www.instagram.com/SomosMas/'}>
+      <a href={instagram || 'https://www.instagram.com/SomosMas/'}>
         <img src={InstagramIcon} alt='instagram_icon' />
       </a>
-      <a href={'https://www.facebook.com/Somos_Mas/'}>
+      <a href={facebook || 'https://www.facebook.com/Somos_Mas/'}>
         <img src={FacebookIcon} alt='facebook_icon' />
       </a>
-      <a href={'https://wa.link/eb1d6x'}>
+      <a href={linkedin || 'https://wa.link/eb1d6x'}>
         <img src={WhatsappIcon} alt='whatsapp_icon' />{' '}
       </a>
     </div>
   );
 };
 
-//This fetch implementation should be replaced with corresponding Axios instance / API Service
-async function fetchSocialMediaLinks() {
-  try {
-    const response = await fetch(`http://${window.location.hostname}:3000/api/organization/public`);
-    const jsonResponse = await response.json();
-    const { instagram, linkedin, facebook } = jsonResponse;
-    return { instagram, linkedin, facebook };
-  } catch (error) {
-    console.log(error);
-  }
-}
 export default RedesIcons;
