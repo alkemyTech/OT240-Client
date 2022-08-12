a110a30e1068d6297c5a24fde48d00f4ce603a26
Mauricio <mauri.yapura@yahoo.com>
2022-08-12 00:15:19 -0300

    implementacion de formulario con tabla de actividades

--- src/components/activities/ActivitiesBackoffice.jsx
+++ src/components/activities/ActivitiesBackoffice.jsx
@@ -1,42 +1,65 @@
 import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import fetchApi from '../../axios/axios';
 import style from "./styles/activitiesBackoffice.module.scss";
 
 
 export const ActivitiesBackoffice = () => {
 
-  let data = [
-      {
-        id: 1,
-        name: "Talleres de lectura",
-        content: "contenido de la actividad 1"      
-      },{
-        id: 2,
-        name: "Programas educativos nivel primaria",
-        content: "contenido de la actividad 2"              
-      },{
-        id: 3,
-        name: "Programas educativos nivel Secundario",
-        content: "contenido de la actividad 3"          
-      },{
-        id: 4,
-        name: "Clases de Ajedrez",
-        content: "contenido de la actividad 4"            
-      }
-  ];
+  const navigate = useNavigate();
+  const location = useLocation();  
+  const [activities, setActivities] = React.useState([]);
 
-  const handleEdit = (e) => {
-    console.log("boton editar")
+  React.useEffect(() => {    
+    const getActivities = async () => {
+      try {
+        const { data } = await fetchApi({ method: 'get', url: '/activities' });
+        setActivities(data);
+      } catch (err) {
+        console.log("Error en el get");        
+      }
+    };
+    getActivities();    
+  }, []);
+
+  const handleEdit = (id, name, content) => {      
+    navigate("editar", {
+      state: {
+        title: 'Editar Actividad',
+        options: { method: 'put', url: `/activities/${id}` },
+        from: location,
+        fields: { name: name, image: '', content: content },
+      },
+    });    
   };
 
-  const handleDelete = () => {
-    console.log("boton eliminar")
-  }
+  const handleDelete = async(id) => {
+    try {      
+      await fetchApi({method: 'delete', url: `/activities/${id}`});
+      alert("Actividad eliminada");
+    } catch (error) {      
+      alert(error.response.data.error)
+    }
+  };
+
+  const handleAdd = () => {
+    navigate("crear", {
+      state: {
+        title: 'Crear Actividad',
+        options: { method: 'post', url: `/activities` },
+        from: location,
+        fields: { name: "", image: '', content: "" },
+      },
+    });
+  };
 
   return (
 
     <div className={style.container}>
-      <h2>Actividades</h2>
-
+      <h2>Actividades</h2>       
+      <div className={style.add}>
+        <button className={style.new} onClick={()=>handleAdd()}>Nueva Actividad</button>
+      </div>
       <table className={style.table}>        
         <thead>
             <tr className={style.tr}>                            
@@ -46,20 +69,18 @@ export const ActivitiesBackoffice = () => {
         </thead>
         <tbody>
           {
-            data.map( (registro)=>(
+            activities.map( (registro)=>(
               <tr key={registro.id} className={style.tr}>
                   <td className={style.name}>{registro.name}</td>                                  
                   <td className={style.options}>
-                      <button className={style.buttonEdit} onClick={()=>handleEdit()}>Editar</button>
-                      <button className={style.buttonDelete} onClick={()=>handleDelete()}>Eliminar</button>
+                      <button className={style.buttonEdit} onClick={()=>handleEdit(registro.id, registro.name, registro.content)}>Editar</button>
+                      <button className={style.buttonDelete} onClick={()=>handleDelete(registro.id)}>Eliminar</button>
                   </td>
               </tr> 
               ) )
           }
         </tbody>      
-      </table>     
-
+      </table>   
     </div>
-
   )
 }
