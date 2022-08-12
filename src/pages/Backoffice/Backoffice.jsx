f201617799ef56370dde2a93701ecd2c866c4cd8
Mauricio <mauri.yapura@yahoo.com>
2022-08-12 00:16:41 -0300

    configuracion de rutas

--- src/pages/Backoffice/Backoffice.jsx
+++ src/pages/Backoffice/Backoffice.jsx
@@ -3,11 +3,11 @@ import Contacts from '../../components/Contacts/Contacts';
 import { Routes, Route } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import BackofficeCards from '../../components/BackofficeCards/BackofficeCards.jsx';
-import { TestimonialBackoffice } from '../../components/testimonials/TestimonialBackoffice';
 import NewsListBackoffice from './NewsListBackoffice/NewsListBackoffice';
 import Categories from '../../components/Categories/Categories';
-import { ActivitiesBackoffice } from '../../components/activities/ActivitiesBackoffice';
 import UsersBackoffice from '../../components/Users/UsersBackoffice';
+import ActivitiesRoutes from './subroutes/ActivityRoutes';
+import TestimonialRoutes from './subroutes/TestimonialRoutes';
 
 const Backoffice = () => {
   return (
@@ -17,10 +17,10 @@ const Backoffice = () => {
       <Routes>
         <Route path='/' element={<BackofficeCards />} />
         <Route path='/novedades/*' element={<NewsListBackoffice />} />
-        <Route path='/testimonios' element={<TestimonialBackoffice />} />
+        <Route path='/testimonios/*' element={<TestimonialRoutes />} />
         <Route path='/categorias' element={<Categories />} />
         <Route path='/contactos' element={<Contacts />} />
-        <Route path='/actividades' element={<ActivitiesBackoffice />} />
+        <Route path='/actividades/*' element={<ActivitiesRoutes />} />
         <Route path='/usuarios' element={<UsersBackoffice />} />
       </Routes>
     </>
