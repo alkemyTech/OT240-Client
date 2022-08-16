e364da47b833d69294382a5dae0a7baeabf18bd0
Andres Nazzari <lordcliff@gmail.com>
2022-08-16 09:31:21 -0300

    added load members to context

--- src/context/orgContext.jsx
+++ src/context/orgContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchUser } from '../redux/actions/auth.action';
 import { loadOrganization } from '../redux/actions/organization.action';
+import { loadMembers } from '../redux/actions/member.actions';
 
 const orgContext = createContext();
 
@@ -17,6 +18,7 @@ const CustomProvider = ({ children }) => {
   useEffect(() => {
     dispatch(fetchUser({ url: '/auth/me' }));
     dispatch(loadOrganization({ method: 'get', url: '/organization/public' }));
+    dispatch(loadMembers({ method: 'get', url: '/members' }));
   }, [dispatch]);
 
   const contextValue = {};
