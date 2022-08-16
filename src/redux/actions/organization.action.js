1628e488341a6cb72fdb766bc65dd85f0ee605fc
Andres Nazzari <lordcliff@gmail.com>
2022-08-16 08:19:31 -0300

    ✨ NEW FEATURE: added load organization

--- src/redux/actions/organization.action.js
+++ src/redux/actions/organization.action.js
@@ -1,4 +1,28 @@
-import { GET_ORGANIZATION } from '../types/organization.types';
+import {
+  GET_ORGANIZATION,
+  PUT_ORGANIZATION,
+  ORGANIZATION_LOADING,
+  ORGANIZATION_SUCCESS,
+  ORGANIZATION_ERROR,
+} from '../types/organization.types';
+import fetchApi from '../../axios/axios';
+
+export const loadOrganization = (options) => async (dispatch) => {
+  dispatch(organizationLoading(true));
+
+  try {
+    const { data } = await fetchApi(options);
+    dispatch(getOrganization(data[0]));
+  } catch (error) {
+    dispatch(organizationError(error.message));
+  } finally {
+    dispatch(organizationLoading(false));
+  }
+};
+
+const getOrganization = (payload) => ({ type: GET_ORGANIZATION, payload });
+const organizationLoading = (payload) => ({ type: ORGANIZATION_LOADING, payload });
+const organizationError = (payload) => ({ type: ORGANIZATION_ERROR, payload });
 
 // demo de funcion
 // export const loadUser = () => async (dispatch) => {
