f3968e1bf805f32a469603d935b2581a5df7f8d7
Andres Nazzari <lordcliff@gmail.com>
2022-08-16 08:19:42 -0300

    ✨ NEW FEATURE: added organization reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import authReducer from './auth.reducer';
 import newsReducer from './news.reducer';
 import formReducer from './form.reducer';
+import organizationReducer from './organization.reducer';
 
 export default combineReducers({
   auth: authReducer,
   news: newsReducer,
   form: formReducer,
+  organization: organizationReducer,
 });
