56f12d726cf90e5ae1b016bdc8894a012f0a09ed
Andres Nazzari <lordcliff@gmail.com>
2022-08-16 08:17:47 -0300

    🎨 IMPROVE: added context, remove auth/me

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-
+import CustomProvider from './context/orgContext.jsx';
 import Public from './pages/Public/Public';
 import Backoffice from './pages/Backoffice/Backoffice';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import { fetchUser } from './redux/actions/auth.action';
 
 function App() {
-  const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    dispatch(fetchUser({ url: '/auth/me' }));
-  }, [dispatch]);
-
   return (
-    <Router>
-      <Routes>
-        <Route path='/*' element={<Public />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/backoffice/*' element={<Backoffice />} />
-        </Route>
-      </Routes>
-    </Router>
+    <CustomProvider>
+      <Router>
+        <Routes>
+          <Route path='/*' element={<Public />} />
+          <Route element={<PrivateRoute />}>
+            <Route path='/backoffice/*' element={<Backoffice />} />
+          </Route>
+        </Routes>
+      </Router>
+    </CustomProvider>
   );
 }
 
