d8d5719293b74b4aeaf3cff488b1fe539e22786f
Andres Nazzari <lordcliff@gmail.com>
2022-08-16 19:38:15 -0300

    context

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import CustomProvider from './context/orgContext.jsx';
 import Public from './pages/Public/Public';
 import Backoffice from './pages/Backoffice/Backoffice';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import { fetchUser } from './redux/actions/auth.action';
 import MyProfileComponent from './pages/MyProfile/MyProfile';
-import PrivateRoute from './routing/PrivateRoute';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/*' element={<Public />} />
-        <Route path='/backoffice/*' element={<PrivateRoute />}>
-          <Route path='/backoffice/*' element={<Backoffice />} />
-        </Route>
-        <Route path='/miperfil/*' element={<MyProfileComponent />} />
-      </Routes>
-    </Router>
+    <CustomProvider>
+      <Router>
+        <Routes>
+          <Route path='/*' element={<Public />} />
+          <Route path='/backoffice/*' element={<PrivateRoute />}>
+            <Route path='/backoffice/*' element={<Backoffice />} />
+          </Route>
+          <Route path='/miperfil/*' element={<MyProfileComponent />} />
+        </Routes>
+      </Router>
+    </CustomProvider>
   );
 }
 
