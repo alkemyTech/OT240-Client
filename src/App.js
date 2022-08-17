d688aa84a86a11fca6b1e5cef416d793a42be366
Andres Nazzari <lordcliff@gmail.com>
2022-08-17 20:25:40 -0300

    🚑 HOTFIX: login and redirects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CustomProvider from './context/OrganizationContext.jsx';
 import Public from './pages/Public/Public';
 import Backoffice from './pages/Backoffice/Backoffice';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
@@ -8,19 +7,17 @@ import MyProfileComponent from './pages/MyProfile/MyProfile';
 
 function App() {
   return (
-    <CustomProvider>
-      <Router>
-        <Routes>
-          <Route path='/*' element={<Public />} />
-          <Route element={<PrivateRoute />}>
-            <Route path='/backoffice/*' element={<Backoffice />} />
-          </Route>
-          <Route element={<PrivateRoute />}>
-            <Route path='/miperfil/*' element={<MyProfileComponent />} />
-          </Route>
-        </Routes>
-      </Router>
-    </CustomProvider>
+    <Router>
+      <Routes>
+        <Route path='/*' element={<Public />} />
+        <Route element={<PrivateRoute allowedRoles={[1]} />}>
+          <Route path='/backoffice/*' element={<Backoffice />} />
+        </Route>
+        <Route element={<PrivateRoute allowedRoles={[1, 2]} />}>
+          <Route path='/miperfil/*' element={<MyProfileComponent />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
