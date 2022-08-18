92606a7f30bb7c5afede36811415109eb88c371b
Alan del Fresno <alandelfresno2@gmail.com>
2022-08-17 22:25:09 -0300

    User redux implementation
    UsersBackoffice reorganized,

--- src/components/Users/UsersBackoffice.jsx
+++ src/components/Users/UsersBackoffice.jsx
@@ -1,45 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import fetchApi from '../../axios/axios';
+import { loadUsers } from '../../redux/actions/user.actions';
 import { handleCreate, handleDelete, handleEdit } from '../../utils/formsHandlers';
 import Table from '../Table/Table';
 import usersBackoffice from './styles/UsersBackoffice.module.scss';
 
 const UsersBackoffice = () => {
-
   const location = useLocation();
   const navigate = useNavigate();
-  const [userData, setUserData] = useState([]);
-  const [getError, setGetError] = useState('Loading...');
+  const dispatch = useDispatch();
+  const { users, loading, error } = useSelector((state) => state.users);
 
   useEffect(() => {
-    const getUsers = async() => {
-      try {
-        const {data} = await fetchApi({method: 'get', url: '/users'});
-        const mapping = data.users.map(({firstName,lastName,email,id,roleId}) => {return({firstName,lastName,email,id,roleId})});
-        setUserData(mapping);
-        setGetError('');
-      } catch (err) {
-        setGetError('Ocurrió un error');
-      };
-    };
-    getUsers();
-  }, []);
+    dispatch(loadUsers({ method: 'get', url: '/users' }));
+  }, [dispatch]);
 
   const editHandler = (user) => {
     handleEdit(navigate, {
-      title: 'Editar Usuario', 
+      title: 'Editar Usuario',
       fields: {
-        firstName: user.firstName, 
-        lastName: user.lastName, 
-        email: user.email, 
-        roleId: user.roleId
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        roleId: user.roleId,
       },
-      options: {method: 'put', url: `/users/${user.id}`},
-      from: location
+      options: { method: 'put', url: `/users/${user.id}` },
+      from: location,
     });
   };
 
@@ -48,34 +38,34 @@ const UsersBackoffice = () => {
       type: 'usuario',
       id: user.id,
       name: `${user.firstName} ${user.lastName}`,
-      url: `/users/${user.id}`
+      url: `/users/${user.id}`,
     });
   };
 
   const createHandler = () => {
     handleCreate(navigate, {
       title: 'Crear usuario',
-      options: {method: 'post', url:'/auth/register'},
+      options: { method: 'post', url: '/auth/register' },
       from: location,
-      fields: {firstName: '', lastName: '', email: '', password: '', roleId: '' }
+      fields: { firstName: '', lastName: '', email: '', password: '', roleId: '' },
     });
   };
 
-  return (
-      getError == '' ? 
-        <Table
-          title='Usuarios'
-          tableHeader={['Nombre', 'Apellido', 'Email']}
-          tableRowsData={userData}
-          tableRowsProperties={['firstName', 'lastName', 'email']}
-          buttons={[
-            {title: 'Editar', handler: editHandler, className: 'orange'},
-            { title: 'Eliminar', handler: deleteHandler, className: 'white'},
-          ]}
-          addBtnHandler={createHandler}
-        />
-      : 
-        <p className={usersBackoffice.errorMessage}> {getError} </p>
+  return error ? (
+    <p className={usersBackoffice.errorMessage}> Algo salió mal..</p>
+  ) : (
+    <Table
+      title='Usuarios'
+      tableHeader={['Nombre', 'Apellido', 'Email']}
+      tableRowsData={users}
+      tableRowsProperties={['firstName', 'lastName', 'email']}
+      buttons={[
+        { title: 'Editar', handler: editHandler, className: 'orange' },
+        { title: 'Eliminar', handler: deleteHandler, className: 'white' },
+      ]}
+      loading={loading}
+      addBtnHandler={createHandler}
+    />
   );
 };
 
