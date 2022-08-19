ed037c8b268ebcf639c3c7fa1ef8ba13c27da746
Andres Nazzari <lordcliff@gmail.com>
2022-08-19 04:47:58 -0300

    💄 UI: added inner html to ckeditor contents

--- src/components/activities/Activity.jsx
+++ src/components/activities/Activity.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import fetchApi from '../../axios/axios';
-import styles from "./styles/activity.module.scss"
-
+import styles from './styles/activity.module.scss';
 
 export const Activity = () => {
+  const { id } = useParams();
+  const [activity, setActivity] = React.useState([]);
 
-    const {id} = useParams();    
-    const [activity, setActivity] = React.useState([]);
+  React.useEffect(() => {
+    const getActivity = async () => {
+      try {
+        const { data } = await fetchApi({ method: 'get', url: `/activities/${id}` });
+        console.log(data);
+        setActivity(data);
+      } catch (err) {
+        console.log('Error en el get');
+      }
+    };
+    getActivity();
+  }, []);
 
-    React.useEffect(() => {    
-        const getActivity = async () => {
-          try {
-            const { data } = await fetchApi({ method: 'get', url: `/activities/${id}` });
-            console.log(data)
-            setActivity(data);
-          } catch (err) {
-            console.log("Error en el get");        
-          }  
-        };
-        getActivity();    
-      }, []);      
-    
-    //let actividad = data.find((el) => el.id==id);       
-      
+  //let actividad = data.find((el) => el.id==id);
+
   return (
-    <div className={styles.container}>  
-        {
-            (activity!=undefined) ? 
-                <>
-                    <img src={activity.image} alt='actividad' className={styles.image} /> 
-                    <h2 className={styles.name}>{activity.name}</h2> 
-                    <p className={styles.content}>{activity.content}</p> 
-                </>
-                : <p className={styles.error}>Error: no se encontró la actividad</p>                
-            }             
+    <div className={styles.container}>
+      {activity != undefined ? (
+        <>
+          <img src={activity.image} alt='actividad' className={styles.image} />
+          <h2 className={styles.name}>{activity.name}</h2>
+
+          <div
+            className={styles.content}
+            dangerouslySetInnerHTML={{ __html: activity.content } || ''}></div>
+        </>
+      ) : (
+        <p className={styles.error}>Error: no se encontró la actividad</p>
+      )}
     </div>
-  )
-  
-}
+  );
+};
