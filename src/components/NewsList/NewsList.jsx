2088d9e7657d2e1e39ac5b8679a3f85df460f6b2
Mauricio <mauri.yapura@yahoo.com>
2022-08-20 18:56:21 -0300

    added loader on news

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import style from './styles/NewsList.module.scss';
 
 import { fetchNews } from '../../redux/actions/news.actions';
+import { Loader } from '../loader/Loader';
 
 const NewsList = ({ quantity }) => {
   const navigate = useNavigate();
@@ -18,27 +19,29 @@ const NewsList = ({ quantity }) => {
   return (
     <ul className={style.container}>
       {loading ? (
-        <></>
+        <div className={style.loaderDiv}>
+          <Loader />  
+        </div>
       ) : error ? (
         <p className={style.error}>{error}</p>
       ) : !news.length ? (
         <p className={style.empty}>No hay novedades para mostrar</p>
-      ) : (
-        news.slice(0, quantity).map(({ name, image, id }) => (
-          <li className={style.card} key={id}>
-            <div
-              className={style.img}
-              style={{
-                backgroundImage: `url('${image}')`,
-              }}></div>
-            <div>
-              <h1>{name.length > 50 ? name.slice(0, 50) + '...' : name}</h1>
-              <button className={style.buttonLink} onClick={() => navigate(`/novedades/${id}`)}>
-                Ver Novedad
-              </button>
-            </div>
-          </li>
-        ))
+      ) : (         
+          news.slice(0, quantity).map(({ name, image, id }) => (
+            <li className={style.card} key={id}>
+              <div
+                className={style.img}
+                style={{
+                  backgroundImage: `url('${image}')`,
+                }}></div>
+              <div>
+                <h1>{name.length > 50 ? name.slice(0, 50) + '...' : name}</h1>
+                <button className={style.buttonLink} onClick={() => navigate(`/novedades/${id}`)}>
+                  Ver Novedad
+                </button>
+              </div>
+            </li>
+          ))        
       )}
     </ul>
   );
