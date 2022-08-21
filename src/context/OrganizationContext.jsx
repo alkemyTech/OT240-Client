8c1b5dad0b7b298e3c78f5862a31d9fda16f9723
Tomas <87679143+devtsp@users.noreply.github.com>
2022-08-21 18:43:50 -0300

    scroll to top on location changes at app context

--- src/context/OrganizationContext.jsx
+++ src/context/OrganizationContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { fetchUser } from '../redux/actions/auth.action';
 
 const OrganizationContext = createContext();
@@ -12,12 +13,17 @@ export const useOrgContext = () => {
 
 const CustomProvider = ({ children }) => {
   const dispatch = useDispatch();
+  const location = useLocation();
 
   useEffect(() => {
     if (sessionStorage.getItem('token')) {
       dispatch(fetchUser({ url: '/auth/me' }));
-    };
+    }
   }, [dispatch]);
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   const contextValue = {};
   return <Provider value={contextValue}>{children}</Provider>;
